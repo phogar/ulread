5b0a4cc93f36eaaa5d87c0ab14c48aea806c1eda
Marcos Vives Del Sol <socram8888@gmail.com>
2015-02-25 10:27:15 +0100

    Use enum instead of defines

--- structs.h
+++ structs.h
@@ -5,30 +5,32 @@
 #include <stdint.h>
 #include "ultralight.h"
 
-#define CMD_GET_VERSION	0x60
-#define CMD_READ		0x30
-#define CMD_FAST_READ	0x3A
-#define CMD_WRITE		0xA2
-#define CMD_COMP_WRITE	0xA0
-#define CMD_READ_CNT	0x39
-#define CMD_PWD_AUTH	0x1B
-#define CMD_READ_SIG	0x3C
+typedef enum {
+	CMD_PWD_AUTH	= 0x1B,
+	CMD_READ		= 0x30,
+	CMD_READ_CNT	= 0x39,
+	CMD_FAST_READ	= 0x3A,
+	CMD_READ_SIG	= 0x3C,
+	CMD_GET_VERSION	= 0x60,
+	CMD_COMP_WRITE	= 0xA0,
+	CMD_WRITE		= 0xA2,
+} ul_cmd;
 
 #pragma pack(1)
 
 typedef struct {
-	uint8_t command;
+	ul_cmd command;
 	uint8_t address;
 } read_request;
 
 typedef struct {
-	uint8_t command;
+	ul_cmd command;
 	uint8_t address;
 	ul_page data[UL_WRPAGS];
 } write_request;
 
 typedef struct {
-	uint8_t command;
+	ul_cmd command;
 	uint8_t dummy[0];
 } version_request;
 
@@ -44,7 +46,7 @@ typedef struct {
 } version_response;
 
 typedef struct {
-	uint8_t command;
+	ul_cmd command;
 	uint8_t address;
 } signature_request;
 
